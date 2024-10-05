a3298040ee9f1f0910781e6f8b99e3ac18809740
xrc2 <109512837+9xbt@users.noreply.github.com>
2024-10-06 00:56:55 +0200

    incredible features

--- src/xwm.c
+++ src/xwm.c
@@ -7,13 +7,68 @@
 
 static bool running = true;
 
+void frame(Window wnd) {
+    XWindowAttributes attribs;
+    if (!XGetWindowAttributes(_display, wnd, &attribs))
+        printf("xwm: warning: failed to get window attributes\n");
+
+    /* don't blame me for this spaghetti 1am code that isn't even finished */
+
+    const Window frame = XCreateSimpleWindow(
+        _display,
+        frame,
+        attribs.x,
+        attribs.y,
+        attribs.width,
+        attribs.height,
+        border_width,
+        border_color,
+        bg_color
+    );
+
+    XSelectInput(
+        _display,
+        frame,
+        SubstructureRedirectMask | SubstructureNotifyMask
+    );
+
+    XAddToSaveSet(_display, wnd);
+
+    XReparentWindow(
+        _display,
+        wnd,
+        frame,
+        0, 0
+    );
+
+    XMapWindow(_display, frame);
+}
+
+void configurerequest(XConfigureRequestEvent event) {
+    XWindowChanges changes;
+    changes.x = event.x;
+    changes.y = event.y;
+    changes.width = event.width;
+    changes.height = event.height;
+    changes.border_width = event.border_width;
+    changes.sibling = event.above;
+    changes.stack_mode = event.detail;
+    
+    XConfigureWindow(_display, event.window, event.value_mask, &changes);
+}
+
+void maprequest(XMapRequestEvent event) {
+    frame(event.window);
+    XMapWindow(_display, event.window);
+}
+
 void die(const char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
     int ret = vfprintf(stderr, fmt, args);
     va_end(args);
 
-    exit(1);
+    exit(EXIT_FAILURE);
 }
 
 int wm_running(Display *display, XErrorEvent *err) {
@@ -30,8 +85,16 @@ void run(void) {
     XSync(_display, False);
     while (running && !XNextEvent(_display, &event)) {
         switch (event.type) {
+            case ConfigureRequest:
+                configurerequest(event.xconfigurerequest);
+                break;
+            case MapRequest:
+                maprequest(event.xmaprequest);
+                break;
+            case CreateNotify:
+                break;
             default:
-                printf("xwm: warning: unknown event type %d", event.type);
+                printf("xwm: warning: ignored event type %d", event.type);
                 break;
         }
     }
--- src/xwm.h
+++ src/xwm.h
@@ -1,3 +1,15 @@
 #include <X11/Xlib.h>
 
 Display *_display;
+
+const unsigned int border_width = 1;
+const unsigned long border_color = 0x005577;
+const unsigned long bg_color = 0x222222;
+
+void frame(Window wnd);
+void configurerequest(XConfigureRequestEvent event);
+void maprequest(XMapRequestEvent event);
+void die(const char *fmt, ...);
+int wm_running(Display *display, XErrorEvent *err);
+int xerror(Display *_display, XErrorEvent *err);
+void run(void);
